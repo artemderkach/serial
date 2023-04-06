c51bf974ee5f3842873c50fa52df98afaf200fad
Artem Derkach <artem.derkach.gs@gmail.com>
2023-04-06 14:44:52 +0300

    update structure of program

--- main.zig
+++ main.zig
@@ -11,14 +11,14 @@ pub fn main() void {
     const fd = os.open("/dev/ttyACM0", os.O.RDONLY, 0) catch unreachable;
     defer os.close(fd);
 
+    // termios structure is used for configuring ports
     var termios = os.tcgetattr(fd) catch unreachable;
 
     termios.cflag &= ~@as(std.os.linux.tcflag_t, 0o000000010017);
     termios.cflag |= os.linux.B115200;
+    termios.cflag |= std.os.linux.CS8;
     termios.ispeed = os.linux.B115200;
     termios.ospeed = os.linux.B115200;
-    termios.cflag |= std.os.linux.CS8;
-
     termios.cc[VMIN] = 1;
     termios.cc[VSTOP] = 0x13; // XOFF
     termios.cc[VSTART] = 0x11; // XON
@@ -26,29 +26,38 @@ pub fn main() void {
 
     os.tcsetattr(fd, os.TCSA.NOW, termios) catch unreachable;
 
+    // flush port
     _ = std.os.linux.syscall3(.ioctl, @bitCast(usize, @as(isize, fd)), 0x540B, 2);
 
-    var buf: [100]u8 = undefined;
-    // const n = try serial.reader().readUntilDelimiterOrEof(&buf, '\n');
-
-    // std.fs
+    // limit for one line in terminal
+    var buf: [256]u8 = undefined;
 
     while (true) {
-        var index: usize = 0;
-        while (true) {
-            var byte: [1]u8 = undefined;
-            _ = os.read(fd, &byte) catch unreachable;
-
-            buf[index] = byte[0];
-
-            if (byte[0] == '\n') {
-                break;
-            }
-
-            index += 1;
-        }
+        var index = readBytes(fd, &buf);
 
         std.debug.print("{s}", .{buf[0 .. index + 1]});
         buf = undefined;
     }
+}
+
+// read bytes until new line delimiter
+fn readBytes(fd: os.fd_t, buf: []u8) usize {
+    var index: usize = 0;
+    var byte: [1]u8 = undefined;
+
+    while (true) {
+        byte = undefined;
+
+        _ = os.read(fd, &byte) catch unreachable;
+
+        buf[index] = byte[0];
+
+        if (byte[0] == '\n') {
+            break;
+        }
+
+        index += 1;
+    }
+
+    return index;
 }
